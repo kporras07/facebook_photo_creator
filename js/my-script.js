8a4633925054c041525fe8c078a226e79ec82022
Kevin Porras <kporras07@gmail.com>
2014-05-08 18:40:05 -0600

    Ajustes de estilos

--- js/my-script.js
+++ js/my-script.js
@@ -1,22 +1,9 @@
 $(document).ready(function(){
     $('#fileupload').bootstrapFileInput();
     $('.file-input-wrapper').removeClass('btn-default');
+    var span = $('#add-buttons').children(':first-child');
+    span.removeClass('glyphicon');
+    span.removeClass('glyphicon-plus');
+    span.children('a').addClass('glyphicon');
+    span.children('a').addClass('glyphicon-plus');
 });
-$('#s1').click(function(){
-    c.setWidth(851);
-    c.setHeight(397);
-    c.setOverlayImage('img/foreground-personal.png', c.renderAll.bind(c));
-    $('.st').removeClass('active');
-    $('#timeline_header').removeClass('switch_gplus').addClass('switch_fb');
-    $(this).addClass('active');
-    set = 'personal';
-});
-$('#s2').click(function(){
-    c.setWidth(851);
-    c.setHeight(397);
-    c.setOverlayImage('img/foreground-page.png', c.renderAll.bind(c));
-    $('.st').removeClass('active');
-    $('#timeline_header').removeClass('switch_gplus').addClass('switch_fb');
-    $(this).addClass('active');
-    set = 'page';
-}); 
